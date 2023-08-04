6bbc562313bb1a7466100e61736ef681405ba496
Gallinocoq <defcoq73@yahoo.fr>
2023-08-04 16:57:53 +0200

    publish multi-country

--- lib/app/modules/root/views/country_selection_view.dart
+++ lib/app/modules/root/views/country_selection_view.dart
@@ -34,6 +34,69 @@ class CountrySelectionView extends GetView<CountrySelectionController> {
                   },
                 );
               }).toList(),
+            )),
+            ElevatedButton(
+              onPressed: () async {
+                if (controller.selectedClient.isNotEmpty) {
+                  // Show loading indicator
+                  showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    builder: (BuildContext context) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    },
+                  );
+
+                  // Perform initialization in the background
+                  await controller.initServices();
+
+                  // Dismiss the loading indicator
+                  Navigator.pop(context);
+
+                  // Navigate to the root page
+                  Get.toNamed(Routes.ROOT);
+                } else {
+                  Get.snackbar('Error', 'Please select a zone');
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*class CountrySelectionView extends GetView<CountrySelectionController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select TresseChezToi Zone'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Please select your TresseChezToi Zone:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Obx(() => Column(
+              children: controller.clients.keys.map((client) {
+                return RadioListTile<String>(
+                  title: Text(controller.getDescription(client)),
+                  value: client,
+                  groupValue: controller.selectedClient.value,
+                  onChanged: (selectedClient) {
+                    controller.setSelectedClient(selectedClient!);
+                  },
+                );
+              }).toList(),
             )
             ),
 
@@ -54,4 +117,4 @@ class CountrySelectionView extends GetView<CountrySelectionController> {
       ),
     );
   }
-}
+}*/
