0503b109324c69f652234ddc8aba1cbdcba5484b
Gallinocoq <defcoq73@yahoo.fr>
2023-08-06 18:48:17 +0200

    correct google map problem adding api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyALp9Ytj6DEaZng5_su109nvEr9rfYPEJw")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
